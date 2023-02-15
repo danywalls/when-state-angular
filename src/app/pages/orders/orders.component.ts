173b98c37b28660bd78afd4c19be90a547c8e39f
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-02-15 19:45:07 +0100

    refactor(orders): Simplify observability
    Use observables to react to changes.

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,46 +1,28 @@
 import {PaymentsService} from '../../services/payments.service';
-import {Component, OnDestroy} from '@angular/core';
+import {Component} from '@angular/core';
 import {OrdersService} from 'src/app/services/orders.service';
-import {takeWhile, tap} from 'rxjs/operators';
+import {startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css'],
 })
-export class OrdersComponent implements OnDestroy {
+export class OrdersComponent {
   orders$ = this.ordersService.orders$;
-  currentBalance = 0;
-  private alive = true;
+  currentBalance = this.paymentService.paymentBalance$.pipe(startWith(0))
+
 
   constructor(
     private ordersService: OrdersService,
     private paymentService: PaymentsService
   ) {
-    this.registerListeners();
-  }
 
-  ngOnDestroy(): void {
-    this.alive = false;
-  }
-
-  registerListeners(): void {
-    this.paymentService.paymentBalance$
-      .pipe(
-        takeWhile(() => this.alive),
-        tap((value: number) => {
-          this.currentBalance = value
-        })
-      )
-      .subscribe()
   }
 
   addOrder(order: HTMLInputElement) {
-    const validPurchase = order.value && this.currentBalance >= 0
-    if (validPurchase) {
-      this.ordersService.addOrder(order.valueAsNumber);
-      this.paymentService.updateBalance(1);
-      order.value = '';
-    }
+    this.ordersService.addOrder(order.valueAsNumber);
+    this.paymentService.updateBalance(1);
+    order.value = '';
   }
 }
