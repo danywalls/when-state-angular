f54861bee1fb6081b599b41b4bf32970aa8a8d0a
Dany Paredes <dany.paredes@external.wolterskluwer.com>
2023-02-15 15:50:06 +0100

    feat(orders): discount balance when add order
    substract the balance when user add a new order

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,25 +1,43 @@
-import { PaymentsService } from './../../services/payments.service';
-import { Component, OnInit } from '@angular/core';
-import { OrdersService } from 'src/app/services/orders.service';
-import { combineLatest } from 'rxjs';
+import {PaymentsService} from '../../services/payments.service';
+import {Component, OnDestroy} from '@angular/core';
+import {OrdersService} from 'src/app/services/orders.service';
+import {takeWhile, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css'],
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnDestroy {
   orders$ = this.ordersService.orders$;
-
+  currentBalance = 0;
+  private alive = true;
 
   constructor(
     private ordersService: OrdersService,
     private paymentService: PaymentsService
-  ) { }
+  ) {
+    this.registerListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
+
+  registerListeners(): void {
+    this.paymentService.paymentBalance$
+      .pipe(
+        takeWhile(() => this.alive),
+        tap((value: number) => {
+          this.currentBalance = value
+        })
+      )
+      .subscribe()
+  }
 
   addOrder(order: HTMLInputElement) {
-
-    if (order.value) {
+    const validPurchase = order.value && this.currentBalance >= 0
+    if (validPurchase) {
       this.ordersService.addOrder(order.valueAsNumber);
       this.paymentService.updateBalance(1);
       order.value = '';
